182657629f81e4a164804bd6d378b92acd1ee713
Norman Yap <normanyaptw@yahoo.com>
2023-04-27 05:11:18 +1000

    modified cors function

--- hylo-backend/app.js
+++ hylo-backend/app.js
@@ -6,7 +6,10 @@ const connectDB = require('./db/connect')
 const port = process.env.PORT || 5000
 const cors = require('cors')
 
-app.use(cors())
+app.use(cors({
+    origin: "*",
+    credentials: true
+}))
 app.use(cookieParser())
 
 // import routers
